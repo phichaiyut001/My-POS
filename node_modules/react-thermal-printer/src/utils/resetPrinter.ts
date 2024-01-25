9935e58f9046211542a1e5877921af68296c3ebf
phichaiyut001 <pichayutkingpila@gmail.com>
2024-01-25 18:34:04 +0700

    Removed node_modules folder

--- node_modules/react-thermal-printer/src/utils/resetPrinter.ts
+++
@@ -1,5 +0,0 @@
-import { Printer } from '@react-thermal-printer/printer';
-
-export function resetPrinter(printer: Printer) {
-  printer.setTextNormal().setTextSize(1, 1).setAlign('left').invert(false);
-}
